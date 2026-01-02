fe3ed0e551893f31c760bf9c80ed2474d5c556e2
fabricio <fabriciosantosc11@gmail.com>
2026-01-02 15:56:07 -0300

    .

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,12 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
+
 
 import {Home} from './pages/home'
 import {Detail} from './pages/detail'
 import {NotFound} from './pages/notfound'
 import { Layout } from "./components/layout";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         element: <Layout/>,
         children: [
